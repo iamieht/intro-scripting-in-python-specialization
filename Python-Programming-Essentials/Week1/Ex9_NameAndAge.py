c83abb58922374a08c198bcfe52fffed0dc42def
Iván E. Hernández T <iamieht@gmail.com>
2020-02-26 17:05:22 +0100

    Exercise 9

---
+++ Python-Programming-Essentials/Week1/Ex9_NameAndAge.py
@@ -0,0 +1,13 @@
+"""
+Compute the statement about a person's name and age, given the person's name and age.
+"""
+###################################################
+# Name and age formula
+# Student should enter statement on the next line.
+
+print("Joe Warren " + "is " + str(56) + " years old.") 
+###################################################
+# Expected output
+# Student should look at the following comments and compare to printed output.
+
+#Joe Warren is 56 years old.
