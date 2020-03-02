d1bc5ebedfc0c93d11d75fa0d4c86db22725df5f
Iván E. Hernández T <iamieht@gmail.com>
2020-03-02 19:58:20 +0100

    Week3 Exercise 1

---
+++ Python-Programming-Essentials/Week3/Ex1_W3_is_even.py
@@ -0,0 +1,44 @@
+"""
+Compute whether an integer is even.
+"""
+
+###################################################
+# Is even formula
+# Student should enter function on the next lines.
+def is_even(number):
+    '''
+    Returns True if number is even or False if not
+    '''
+    return number % 2 == 0
+
+
+
+###################################################
+# Tests
+# Student should not change this code.
+
+number = 8
+if is_even(number):
+    print(number, "is even.")
+else:
+    print(number, "is odd.")
+    
+number = 3
+if is_even(number):
+    print(number, "is even.")
+else:
+    print(number, "is odd.")
+    
+number = 12
+if is_even(number):
+    print(number, "is even.")
+else:
+    print(number, "is odd.")
+
+###################################################
+# Expected output
+# Student should look at the following comments and compare to printed output.
+
+#8 is even.
+#3 is odd.
+#12 is even.
