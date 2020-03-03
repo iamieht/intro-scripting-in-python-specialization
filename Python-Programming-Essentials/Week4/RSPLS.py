65e006259e81722aa54346dcf54317304c87b83c
Iván E. Hernández T <iamieht@gmail.com>
2020-03-03 21:01:43 +0100

    Week4 - RSPLS - number_to_name function

--- Python-Programming-Essentials/Week4/RSPLS.py
+++ Python-Programming-Essentials/Week4/RSPLS.py
@@ -41,11 +41,18 @@ def number_to_name(number):
     Given integer number (0, 1, 2, 3, or 4)
     corresponding name from video
     """
-    
-    # convert number to a name using if/elif/else
-    # don't forget to return the result!
-    
-    pass
+    if number == 0:
+        return 'rock'
+    elif number == 1:
+        return 'Spock'
+    elif number == 2:
+        return 'paper'
+    elif number == 3:
+        return 'lizard'
+    elif number == 4:
+        return 'scissors'
+    else:
+        return "Error: Bad Input"
 
 
 def rpsls(player_choice):
