2bce8fe8838b9957c0da1d70021d44fe48100b2d
Iván E. Hernández T <iamieht@gmail.com>
2020-03-03 21:19:46 +0100

    Week4 - RSPLS - full game

--- Python-Programming-Essentials/Week4/RSPLS.py
+++ Python-Programming-Essentials/Week4/RSPLS.py
@@ -62,22 +62,29 @@ def rpsls(player_choice):
     """
     
     # print a blank line to separate consecutive games
+    print('')
     
     # print out the message for the player's choice
+    print('Player chooses', player_choice)
 
     # convert the player's choice to player_number using the function name_to_number()
-
+    player_number = name_to_number(player_choice)
+    
     # compute random guess for comp_number using random.randrange()
-
+    comp_number = random.randrange(0, 4)
     # convert comp_number to comp_choice using the function number_to_name()
-    
+    comp_choice = number_to_name(comp_number)
     # print out message for computer's choice
-
+    print('Computer chooses', comp_choice)
     # compute difference of player_number and comp_number modulo five
-
+    difference = (comp_number - player_number) % 5
     # use if/elif/else to determine winner and print winner message
-    
-    pass
+    if difference == 1 or difference == 2:
+        print("Computer wins!")
+    elif difference == 3 or difference == 4:
+        print("Player wins!")
+    else:
+        print("Player and computer tie!")
      
     
 # test your code
