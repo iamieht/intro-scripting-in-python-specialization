a31ebd4caa3c53c5737af8c662acb5230e1c4aa8
Iván E. Hernández T <iamieht@gmail.com>
2020-03-10 21:41:22 +0100

    Week2 - Exercise 1

---
+++ Python-Data-Representations/Week2/Ex1_W2_prime_list.py
@@ -0,0 +1,12 @@
+"""
+Create a list of the first six primes and print the 2nd, 4th, and 6th
+"""
+
+# Enter code here
+
+prime_list = [2, 3, 5, 7, 11, 13]
+print(prime_list[1], prime_list[3], prime_list[-1])
+
+
+# Output
+#3 7 13
