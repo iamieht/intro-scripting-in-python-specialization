43a9e1509304568106bb781fcded231656355e1f
Iván E. Hernández T <iamieht@gmail.com>
2020-03-11 20:09:51 +0100

    Week2 - Exercise 2

---
+++ Python-Data-Representations/Week2/Ex2_W2_first_last.py
@@ -0,0 +1,14 @@
+"""
+Create a list formed by the first and last items of example_list
+"""
+
+example_list = [2, 3, 5, 7, 11, 13]
+
+# Uncomment and complete
+
+firstlast_list = [example_list[0], example_list[-1]]
+print(firstlast_list)
+
+
+# Output
+#[2, 13]
