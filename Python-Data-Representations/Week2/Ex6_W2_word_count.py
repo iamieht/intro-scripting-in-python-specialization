541060be8be7b654761dfd533db5147ccd2f0fd7
Iván E. Hernández T <iamieht@gmail.com>
2020-03-11 20:19:54 +0100

    Week2 - Exercise 6

---
+++ Python-Data-Representations/Week2/Ex6_W2_word_count.py
@@ -0,0 +1,24 @@
+"""
+Count the number of times that a word appears in string of text
+"""
+
+def word_count(text, word):
+    """
+    Given a string text consist of words separate by spaces and a string word
+    (with no spaces), return the number of times that word appears in the text
+    """
+    
+    words = text.split()
+    return words.count(word)
+
+
+# Tests
+
+print(word_count("this pigdog is a fine pigdog", "pigdog"))
+print(word_count("this pigdog is not a dog", "dog"))
+print(word_count("this pigdog is not a pig", "pigdog"))
+
+# Output
+#2
+#1
+#1
