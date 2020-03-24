db8f006add5dfd7ec3f08a9e7af5bd0931dfa8f6
Iván E. Hernández T <iamieht@gmail.com>
2020-03-24 20:26:30 +0100

    Week3 - Exercise 1

---
+++ Python-Data-Representations/Week3/Ex1_W3_list_assign.py
@@ -0,0 +1,15 @@
+"""
+Update an item in a list
+"""
+
+example_list = [2, 3, 5, 7, 11, 13]
+print(example_list)
+
+example_list[2] = 0
+
+print(example_list)
+
+
+# Output
+#[2, 3, 5, 7, 11, 13]
+#[2, 3, 0, 7, 11, 13]
