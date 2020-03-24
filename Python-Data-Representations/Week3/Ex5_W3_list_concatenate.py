6741fc5aa05bbf88901039c2ff53991ffd69a6d1
Iván E. Hernández T <iamieht@gmail.com>
2020-03-24 20:33:59 +0100

    Week3 - Exercise 5

---
+++ Python-Data-Representations/Week3/Ex5_W3_list_concatenate.py
@@ -0,0 +1,17 @@
+"""
+Concatenate one list onto another
+"""
+
+example_list = [2, 3, 5, 7, 11, 13]
+print(example_list)
+
+new_list = example_list + [0,0,0]
+
+print(example_list)
+print(new_list)
+
+
+# Output
+#[2, 3, 5, 7, 11, 13]
+#[2, 3, 5, 7, 11, 13]
+#[2, 3, 5, 7, 11, 13, 0, 0, 0]
