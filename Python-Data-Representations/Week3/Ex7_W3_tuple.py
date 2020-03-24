a20fe93a46921b31c74b12c21290fb5b490acfac
Iván E. Hernández T <iamieht@gmail.com>
2020-03-24 20:37:23 +0100

    Week3 - Exercise 7

---
+++ Python-Data-Representations/Week3/Ex7_W3_tuple.py
@@ -0,0 +1,15 @@
+"""
+Convert a list to a tuple
+"""
+
+example_list = [2, 3, 5, 7, 11, 13]
+print(example_list)
+
+example_tuple = tuple(example_list)
+    
+print(example_tuple)
+
+
+# Output
+#[2, 3, 5, 7, 11, 13]
+#(2, 3, 5, 7, 11, 13)
