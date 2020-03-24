766f51eb7a163ba7520a6b98a0458a7bb911d5b4
Iván E. Hernández T <iamieht@gmail.com>
2020-03-24 20:31:58 +0100

    Week3 - Exercise 4

---
+++ Python-Data-Representations/Week3/Ex4_W3_list_extend.py
@@ -0,0 +1,15 @@
+"""
+Extend a list with another list
+"""
+
+example_list = [2, 3, 5, 7, 11, 13]
+print(example_list)
+
+example_list.extend([0,0,0])
+
+print(example_list)
+
+
+# Output
+#[2, 3, 5, 7, 11, 13]
+#[2, 3, 5, 7, 11, 13, 0, 0, 0]
