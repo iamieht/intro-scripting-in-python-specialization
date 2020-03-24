201a9d8d1166a9fc8eb68566256bdb6ca2bf9840
Iván E. Hernández T <iamieht@gmail.com>
2020-03-24 20:30:24 +0100

    Week3 - Exercise 3

---
+++ Python-Data-Representations/Week3/Ex3_W3_list_append.py
@@ -0,0 +1,15 @@
+"""
+Append an item to a list
+"""
+
+example_list = [2, 3, 5, 7, 11, 13]
+print(example_list)
+
+example_list.append(0)
+
+print(example_list)
+
+
+# Output
+#[2, 3, 5, 7, 11, 13]
+#[2, 3, 5, 7, 11, 13, 0]
