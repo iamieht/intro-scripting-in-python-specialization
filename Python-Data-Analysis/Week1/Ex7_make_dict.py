24147101888b1435b393449a0ab950faf7bc369e
Iván E. Hernández T <iamieht@gmail.com>
2020-04-16 21:02:24 +0200

    Week1 - Ex7 - make_dict

---
+++ Python-Data-Analysis/Week1/Ex7_make_dict.py
@@ -0,0 +1,25 @@
+"""
+Write a function make_dict(key_value_list) that
+takes a list of tuples (key, value) and returns a 
+dictionary with these keys and values
+"""
+
+
+def make_dict(key_value_list):
+    """
+    Given a list of tuples of the form (key, value), 
+    return a dictionary with the corresponding keys and values
+    """
+    
+    # Enter code here
+    return dict(key_value_list)
+
+# Testing code
+print(make_dict([]))
+print(make_dict([(0, 1)]))
+print(make_dict([("Joe", 1), ("Scott", 2), ("John", 4)]))
+
+# Output
+#{}
+#{0: 1}
+#{'John': 4, 'Joe': 1, 'Scott': 2}
