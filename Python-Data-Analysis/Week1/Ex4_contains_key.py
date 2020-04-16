60d8aaa29e815a1c7f6f40d786a38f516e8f011b
Iván E. Hernández T <iamieht@gmail.com>
2020-04-16 20:23:09 +0200

    Week1 - Ex4 - Contains Key

---
+++ Python-Data-Analysis/Week1/Ex4_contains_key.py
@@ -0,0 +1,21 @@
+"""
+Write an expression that determines whether
+a key is in a dictionary
+"""
+
+# Initialize dictionary
+my_dict = {"Joe" : 1, "Scott" : 2, "John" : 3}
+
+# Print True/False depending on whether the key "Joe" is in my_dict
+print("Joe" in my_dict)
+
+# Print True/False depending on whether the key "John" is in my_dict
+print("John" in my_dict)
+
+# Print True/False depending on whether the key "Stephen" is in my_dict
+print("Stephen" in my_dict)
+
+# Output
+#True
+#True
+#False
