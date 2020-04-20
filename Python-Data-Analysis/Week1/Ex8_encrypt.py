e55d5ff5ca7c7525ab3ec7747da17c8e136ad5d5
Iván E. Hernández T <iamieht@gmail.com>
2020-04-20 21:54:13 +0200

    Week1 - Ex8 - encrypt

---
+++ Python-Data-Analysis/Week1/Ex8_encrypt.py
@@ -0,0 +1,37 @@
+"""
+part 1
+Using substitution ciphers to encrypt and decrypt plain text
+"""
+
+
+# Part 1 - Use a dictionary that represents a substition cipher to 
+# encrypt a phrase
+
+# Example of a cipher dictionary 26 lower case letters plus the blank
+CIPHER_DICT = {'e': 'u', 'b': 's', 'k': 'x', 'u': 'q', 'y': 'c', 'm': 'w', 'o': 'y', 'g': 'f', 'a': 'm', 'x': 'j', 'l': 'n', 's': 'o', 'r': 'g', 'i': 'i', 'j': 'z', 'c': 'k', 'f': 'p', ' ': 'b', 'q': 'r', 'z': 'e', 'p': 'v', 'v': 'l', 'h': 'h', 'd': 'd', 'n': 'a', 't': ' ', 'w': 't'}
+
+def encrypt(phrase, cipher_dict):
+    """
+    Take a string phrase (lower case plus blank) 
+    and encypt it using the dictionary cipher_dict
+    """
+    encrypted_phrase = ''
+
+    for char in phrase:
+        encrypted_phrase += cipher_dict.get(char)
+    
+    return encrypted_phrase
+
+
+# Tests
+print("Output for part 1")
+print(encrypt("pig", CIPHER_DICT))
+print(encrypt("hello world", CIPHER_DICT))
+print()
+
+# Output for part 1
+#vif
+#hunnybtygnd
+
+
+
