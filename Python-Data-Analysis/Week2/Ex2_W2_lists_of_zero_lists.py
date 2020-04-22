5e7e70a16a70b215e908b6b743519b907a60999b
Iván E. Hernández T <iamieht@gmail.com>
2020-04-22 21:10:49 +0200

    Week2 - Ex2 - list of zero lists

---
+++ Python-Data-Analysis/Week2/Ex2_W2_lists_of_zero_lists.py
@@ -0,0 +1,13 @@
+"""
+Create a list nested_list of length 5 whose items 
+are themselves lists consisting of 3 zeros
+"""
+
+nested_list = [[0,0,0], [0,0,0], [0,0,0], [0,0,0], [0,0,0]]
+
+
+# Tests
+print(nested_list)
+
+# Output
+#[[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]]
