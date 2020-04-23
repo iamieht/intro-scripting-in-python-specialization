76e7364faff7a3027b3359a7e004afec82b9f2c8
Iván E. Hernández T <iamieht@gmail.com>
2020-04-23 21:05:30 +0200

    Week2 - Ex9 - grade table

---
+++ Python-Data-Analysis/Week2/Ex9_W2_grade_table.py
@@ -0,0 +1,24 @@
+"""
+Create a dictionary grade_table whose keys are provided
+student names and values are associated list of grades
+"""
+
+
+# Add code here
+grade_table = {"Joe": [100, 98, 100, 13],
+                "Scott": [75, 59, 89, 77],
+                "John": [86, 84, 91, 78]}
+
+
+# Tests
+print(grade_table["Joe"])
+print(grade_table["Scott"])
+print(grade_table["John"])
+
+
+# Output
+#[100, 98, 100, 13]
+#[75, 59, 89, 77]
+#[86, 84, 91, 78]
+
+
