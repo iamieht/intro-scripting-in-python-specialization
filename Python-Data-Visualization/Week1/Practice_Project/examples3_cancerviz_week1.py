c720ad2b042fd130747fcd3a63795f45b178d740
Iván E. Hernández T <iamieht@gmail.com>
2020-05-26 19:29:31 +0200

    Update examples3_cancerviz_week1.py

--- Python-Data-Visualization/Week1/Practice_Project/examples3_cancerviz_week1.py
+++ Python-Data-Visualization/Week1/Practice_Project/examples3_cancerviz_week1.py
@@ -8,6 +8,7 @@ import matplotlib.pyplot as plt
 # Houston location
 
 USA_SVG_SIZE = [555, 352]
+HOUSTON_POS = [302, 280]
 
 
 def draw_USA_map(map_name):
@@ -17,15 +18,19 @@ def draw_USA_map(map_name):
     """
      
     # Load map image, note that using 'rb'option in open() is critical since png files are binary
-
+    with open(map_name, 'rb') as map_file:        # using 'r' causes Python to crash :(
+        map_img = plt.imread(map_file)
     #  Get dimensions of USA map image
-    
+    ypixels, xpixels, bands = map_img.shape
+    print(xpixels, ypixels, bands)
     # Plot USA map
-    
+    implot = plt.imshow(map_img)
     # Plot green scatter point in center of map
-     
+    plt.scatter(x = xpixels / 2, y = ypixels / 2, s = 100, c = "Green") 
     # Plot red scatter point on Houston, Tx - include code that rescale coordinates for larger PNG files
-    pass
+    plt.scatter(x = HOUSTON_POS[0] * xpixels / USA_SVG_SIZE[0], y = HOUSTON_POS[1] * ypixels / USA_SVG_SIZE[1], s = 100, c = "Red") 
+
+    plt.show()
 
 #draw_USA_map("USA_Counties_555x352.png")
 draw_USA_map("USA_Counties_1000x634.png")   
